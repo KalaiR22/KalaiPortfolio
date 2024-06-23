8094db7ff8426012ff47ec1adaa12438bf2282a1
kalair22 <kalaivaniravi2208@gmail.com>
2024-06-23 12:39:49 +0530

    about

--- src/components/Achievements.jsx
+++ src/components/Achievements.jsx
@@ -1,38 +1,79 @@
-import React from 'react'
-import { achievmentimage } from '../constants';
-import { achievements } from '../constants';
+import React, { useRef, useState } from "react";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+
+import { Autoplay, Pagination } from "swiper/modules";
+import { achievmentimage } from "../constants";
+import { achievements } from "../constants";
 
 const Achievements = () => {
   return (
-    <div className="flex flex-col pb-24">
+    <div className="px-4  sm:px-10 md:px-10 xl:px-14 2xl:px-20 flex flex-col pb-24">
       <div className="mx-auto">
         <h1 className="text-radial-gradient title text-center pb-4">
           Achievements
         </h1>
       </div>
-      <div className="para-1 pt-2  text-center mx-auto leading-6">
+      <div className="para-1 pt-2 px-[1rem] sm:px-[3rem] md:px-[7rem] lg:px-[10rem] xl:px-[13rem] text-center mx-auto leading-6">
         Celebrating key successes and professional highlights and showcasing
-        impactful <br />
-        results and accolades.
+        impactful results and accolades.
       </div>
-      <div className="flex py-28 justify-between">
-        {achievmentimage.map((images) => (
-          <div key={images.id}>
-            <img src={images.image} alt="pic" />
-          </div>
-        ))}
+      <div>
+        <Swiper
+          spaceBetween={30}
+          autoplay={{
+            delay: 2500,
+            disableOnInteraction: false,
+          }}
+          pagination={{
+            clickable: true,
+            bulletClass: "swiper-pagination-bullet",
+            bulletActiveClass: "swiper-pagination-bullet-active",
+          }}
+          breakpoints={{
+            // when window width is >= 320px
+            320: {
+              slidesPerView: 1,
+            },
+            // when window width is >= 640px
+            640: {
+              slidesPerView: 2,
+            },
+            // when window width is >= 1024px
+            1024: {
+              slidesPerView: 3,
+            },
+          }}
+          modules={[Autoplay, Pagination]}
+          className="mySwiper py-10 lg:py-32"
+        >
+          {achievmentimage.map((images) => (
+            <SwiperSlide key={images.id}>
+              <img src={images.image} alt="pic" />
+            </SwiperSlide>
+          ))}
+        </Swiper>
       </div>
-      <div className="px-4 sm:px-10 md:px-28 flex flex-col gap-6 py-10">
+
+      <div className=" sm:px-4 md:px-28 flex flex-col gap-6 py-10">
         {achievements.map((achievement) => (
           <div key={achievement.id}>
-            <div className="text-radial-gradient title ">{achievement.title}</div>
+            <div className="text-radial-gradient title ">
+              {achievement.title}
+            </div>
             <div className="title-7">Year: {achievement.year}</div>
-            <div className="para-7 py-10 leading-7">{achievement.description}</div>
+            <div className="para-7 py-10 leading-7">
+              {achievement.description}
+            </div>
           </div>
         ))}
       </div>
     </div>
   );
-}
+};
 
-export default Achievements
+export default Achievements;
--- src/components/WorkExperience.jsx
+++ src/components/WorkExperience.jsx
@@ -4,7 +4,7 @@ import { workexperience } from '../constants';
 
 const WorkExperience = () => {
   return (
-    <div className=" px-4 sm:px-10 md:px-28 flex flex-col py-16 ">
+    <div className=" px-4  sm:px-10 md:px-10 xl:px-14 2xl:px-20 flex flex-col py-16 ">
       <div className="mx-auto">
         <h1 className="text-radial-gradient title text-center pb-4">
           Work Experience
@@ -14,29 +14,29 @@ const WorkExperience = () => {
         Extensive experience across various roles, demonstrating adaptability
         and growth.
       </div>
-      <div className="flex flex-col gap-36 px-10 py-32">
+      <div className="flex flex-col gap-36 mx-auto md:m-0  mg:px-1 lg:px-5 xl:px-10 py-32">
         {workexperience.map((work) => (
-          <div className="flex justify-between" key={work.id}>
-            <div className="flex gap-4">
-              <div className="pt-[3.4rem]">
-                <img src={ministar} alt="star" />
+          <div className="flex flex-col md:flex-row gap-8  md:justify-between" key={work.id}>
+            <div className="flex justify-center gap-2 sm:gap-4">
+              <div className="  pt-[1rem] md:pt-[2rem] lg:pt-[2.2rem] xl:pt-[3.4rem]">
+                <img src={ministar} alt="star" className='h-[1.5rem] w-[1.5rem] sm:h-[2rem] sm:w-[2rem]' />
               </div>
-              <div className="flex flex-col justify-center leading-[4.8rem]">
+              <div className="flex flex-col justify-center  leading-[3.5rem] lg:leading-[4.8rem]">
                 <div className="titlefour">{work.company}</div>
                 <div className="titlefive">{work.role}</div>
                 <div className="titlefive">{work.part}</div>
               </div>
             </div>
-            <div className="flex flex-col w-[565px] gap-10">
-              <div className="flex bottom-border pb-3 justify-between items-end">
+            <div className="flex flex-col sm:w-[350px] md:w-[330px] lg:w-[460px] xl:w-[565px]  gap-8 lg:gap-10">
+              <div className="flex bottom-border pb-2 xl:pb-3 justify-between items-end">
                 <div className="titlesix">Industry</div>
                 <div className="para-6 pr-4">{work.industry}</div>
               </div>
-              <div className="flex bottom-border pb-3 justify-between items-end">
+              <div className="flex bottom-border pb-2 xl:pb-3 justify-between items-end">
                 <div className="titlesix">Location</div>
                 <div className="para-6 pr-4">{work.location}</div>
               </div>
-              <div className="flex bottom-border pb-3 justify-between items-end">
+              <div className="flex bottom-border pb-2 xl:pb-3 justify-between items-end">
                 <div className="titlesix">Year</div>
                 <div className="para-6 pr-4 ">{work.year}</div>
               </div>
